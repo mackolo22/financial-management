78206f69e9b425829508fb7796d8fecec90d3e67
Maciej <mackolo22@gmail.com>
2021-01-20 12:49:32 +0100

    Added solution and first project.

---
+++ src/FinancialManagment.WebClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FinancialManagment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
